26d81208588889a819f997b4ac88ca734fc30e24
Izzy Simon <ymsimon824@icloud.com>
2017-08-02 15:21:29 +0000

    dog

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,22 @@
 require 'bundler'
 Bundler.require
 
+require_relative './models/dog.rb'
+
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
+  
+  get '/dog' do
+    @dog1 = Dog.new("Fido", "Lab", 5)
+    erb :dog
+  end
+  
+  get '/info' do
+    erb :dog
+  end
+
 
 end
